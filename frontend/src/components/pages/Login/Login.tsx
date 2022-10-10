a5aeea3acf44a6e16484e842ca6b605239e716b6
unknown <datlhdse140718@fpt.edu.vn>
2022-10-10 11:42:04 +0700

    fix layout login page

--- frontend/src/components/pages/Login/Login.tsx
+++ frontend/src/components/pages/Login/Login.tsx
@@ -4,11 +4,14 @@ import "./Login.css";
 const Login: React.FC = props => {
     return(
         <div id="Login">
-            <h1 className="login-title">Vinhomes Grand Park Marketplace</h1>
+            <h1 className="vinhome-title">Vinhomes Grand Park Marketplace</h1>
             <form className="login-form">
+                <h2 className="login-title">Login</h2>
                 <input type="text" className="login-input" placeholder="Username" />
                 <input type="text" className="login-input" placeholder="Password"/>
-                <button className="btn-login">Login</button>
+                <a href="" className="forgot">Forgot Password</a>
+                <button className="btn-login">Login Now</button>
+                <button className="btn-register">Register A New Account</button>
             </form>
         </div>
     );
--- frontend/src/components/pages/Product/Product.tsx
+++ frontend/src/components/pages/Product/Product.tsx
@@ -7,41 +7,40 @@ const Product: React.FC = props => {
                 <a className="link">Trang chủ</a>&nbsp;/&nbsp;<a className="link">Xem tất cả</a>
             </div>
             <div className="border">
-
-            <div className="product-type">
-                <div className="product-img">
-                    <img src="https://demos.buddyboss.com/online-communities/wp-content/sandbox64720-uploads/2019/05/swim-trunk.png" alt="" />
-                </div>
-                <div className="product-summary">
-                    <h1 className="product-name">classic swim trunk</h1>
-                    <div className="product-price">$150.00</div>
-                    <div className="product-description">Swimmer Red 4 Way Stretch 13”</div>
-                    <button className="btn-addtocart">add to cart</button>
-                    <h4>SPECS</h4>
-                    <div className="product-information">
-                        <span className="dimensions-wrapper">
-                            <span className="product-att-title">Dimensions:</span>
-                            <span className="product-att">16 × 9 × 13 in</span>
-                        </span>
-                        <span className="sku-wrapper">
-                            <span className="product-att-title">SKU:</span>
-                            <span className="product-att">HDGHR275JGD</span>
-                        </span>
-                        <span className="category-wrapper">
-                            <span className="product-att-title">Category:</span>
-                            <span className="product-att">Uncategoried</span>
-                        </span>
-                        <span className="tag-wrapper">
-                            <span className="product-att-title">Tag:</span>
-                            <span className="product-att">Swimming wear</span>
-                        </span>
-                        <span className="availability-wrapper">
-                            <span className="product-att-title">Availability:</span>
-                            <span className="product-att">120 in stock</span>
-                        </span>
+                <div className="product-type">
+                    <div className="product-img">
+                        <img src="https://demos.buddyboss.com/online-communities/wp-content/sandbox64720-uploads/2019/05/swim-trunk.png" alt="" />
+                    </div>
+                    <div className="product-summary">
+                        <h1 className="product-name">classic swim trunk</h1>
+                        <div className="product-price">$150.00</div>
+                        <div className="product-description">Swimmer Red 4 Way Stretch 13”</div>
+                        <button className="btn-addtocart">add to cart</button>
+                        <h4>SPECS</h4>
+                        <div className="product-information">
+                            <span className="dimensions-wrapper">
+                                <span className="product-att-title">Dimensions:</span>
+                                <span className="product-att">16 × 9 × 13 in</span>
+                            </span>
+                            <span className="sku-wrapper">
+                                <span className="product-att-title">SKU:</span>
+                                <span className="product-att">HDGHR275JGD</span>
+                            </span>
+                            <span className="category-wrapper">
+                                <span className="product-att-title">Category:</span>
+                                <span className="product-att">Uncategoried</span>
+                            </span>
+                            <span className="tag-wrapper">
+                                <span className="product-att-title">Tag:</span>
+                                <span className="product-att">Swimming wear</span>
+                            </span>
+                            <span className="availability-wrapper">
+                                <span className="product-att-title">Availability:</span>
+                                <span className="product-att">120 in stock</span>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     )
