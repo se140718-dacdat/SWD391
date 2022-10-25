13a99ab89bc59c66bafbd50b0e27cffdd1e28273
se140718 <datlhdse140718@fpt.edu.vn>
2022-10-25 20:05:29 +0700

    fix style create post

--- frontend/src/components/pages/Post/CreatePost.tsx
+++ frontend/src/components/pages/Post/CreatePost.tsx
@@ -7,9 +7,6 @@ const CreatePost: React.FC = props => {
     const [preview, isPreview] = useState<String>("Preview");
     const [post, setPost] = useState<Post>();
     const [select, setSelect] = useState<String>("");
-    const handleChange = (e: FormEvent) => {
-        setSelect(e.target.value);
-    }
     const renderSwitch = () => {
         switch (preview) {
             case "Preview":
@@ -21,7 +18,7 @@ const CreatePost: React.FC = props => {
                             }}>
                                 <input type="file" accept="" id="img-file" style={{ display: "none" }} />
                                 <div className="s6ete4"><svg xmlns="http://www.w3.org/2000/svg" width="53" height="39" viewBox="0 0 53 39">
-                                    <g fill="none" fill-rule="evenodd" stroke="none" stroke-width="1"><g stroke="#FE9900" stroke-width="2" transform="translate(-255 -179)"><g transform="translate(132 122)"><path d="M150.631 87.337c-5.755 0-10.42-4.534-10.42-10.127 0-5.593 4.665-10.127 10.42-10.127s10.42 4.534 10.42 10.127c0 5.593-4.665 10.127-10.42 10.127m10.42-24.755l-2.315-4.501h-16.21l-2.316 4.5h-11.579s-4.631 0-4.631 4.502v22.505c0 4.5 4.631 4.5 4.631 4.5h41.684s4.631 0 4.631-4.5V67.083c0-4.501-4.631-4.501-4.631-4.501h-9.263z"></path></g></g></g></svg><span className="ffyigpx"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 20 21"><g fill="none" fill-rule="evenodd" stroke="none" stroke-width="1"><g fill="#FE9900" transform="translate(-161 -428)"><g transform="translate(132 398)"><g transform="translate(16.648 17.048)"><g transform="rotate(-180 16.142 16.838)"><rect width="2.643" height="19.82" x="8.588" y="0" rx="1.321"></rect><path d="M9.91 0c.73 0 1.321.592 1.321 1.321v17.177a1.321 1.321 0 01-2.643 0V1.321C8.588.591 9.18 0 9.91 0z" transform="rotate(90 9.91 9.91)"></path></g></g></g></g></g></svg></span>
+                                    <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1"><g stroke="#FE9900" strokeWidth="2" transform="translate(-255 -179)"><g transform="translate(132 122)"><path d="M150.631 87.337c-5.755 0-10.42-4.534-10.42-10.127 0-5.593 4.665-10.127 10.42-10.127s10.42 4.534 10.42 10.127c0 5.593-4.665 10.127-10.42 10.127m10.42-24.755l-2.315-4.501h-16.21l-2.316 4.5h-11.579s-4.631 0-4.631 4.502v22.505c0 4.5 4.631 4.5 4.631 4.5h41.684s4.631 0 4.631-4.5V67.083c0-4.501-4.631-4.501-4.631-4.501h-9.263z"></path></g></g></g></svg><span className="ffyigpx"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 20 21"><g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1"><g fill="#FE9900" transform="translate(-161 -428)"><g transform="translate(132 398)"><g transform="translate(16.648 17.048)"><g transform="rotate(-180 16.142 16.838)"><rect width="2.643" height="19.82" x="8.588" y="0" rx="1.321"></rect><path d="M9.91 0c.73 0 1.321.592 1.321 1.321v17.177a1.321 1.321 0 01-2.643 0V1.321C8.588.591 9.18 0 9.91 0z" transform="rotate(90 9.91 9.91)"></path></g></g></g></g></g></svg></span>
                                 </div>
                             </div>
                         </div>
@@ -52,7 +49,11 @@ const CreatePost: React.FC = props => {
                                 <div className="input-title">Stock quantity</div>
                                 <input type="text" className="input-value" />
                             </div>
-                            <div className="btn-wrap">
+                            <div className="input inline-block flex">
+                                <div className="input-title">Description</div>
+                                <textarea className="input-value" />
+                            </div>
+                            <div className="btn-wrap mt-200">
                                 <button className="btn-preview-edit" onClick={() => { (preview) == "Preview" ? isPreview("Edit post") : isPreview("Preview") }}>{preview}</button>
                                 <button className="btn-upload">Upload</button>
                             </div>
